6cceda801110308b8273c088ef43f1e06f85a143
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-03-12 04:04:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// Select the generate button
+const generate = document.querySelector(".generate");
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+// Get all quates
+const quotesArr = [
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote:
+      "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle.",
+    author: "Marilyn Monroe",
+  },
+  {
+    quote: "So many books so little time",
+    author: "Frank Zappa",
+  },
+  {
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: "Albert Einstein",
+  },
+];
+// Creating Event
+const getQuotes = function () {
+  // Get random quoates
+  let randomQuotes = Math.floor(Math.random() * quotesArr.length);
+  quote.textContent = quotesArr[randomQuotes].quote;
+  author.textContent = quotesArr[randomQuotes].author;
+};
+generate.addEventListener("click", getQuotes);
